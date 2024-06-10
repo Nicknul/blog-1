e7a22e0ac8a55b91544d3cc2f66533f09ef4be3d
정호연 <jea7730@gmail.com>
2024-06-10 16:59:20 +0900

    매개변수 없애기

--- blogStr.js
+++ blogStr.js
@@ -1,6 +1,5 @@
 const blogStr = {
-  blog: (title, content) => {
-    `<!DOCTYPE html>
+  blog: `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -9,16 +8,15 @@ const blogStr = {
   </head>
   <body>
     <form action="/submit" method="post">
-      <label for="${title}">제목</label><br />
-      <input type="text" name="${title}" id="${title}" /><br /><br />
-      <label for="${content}">내용</label><br />
-      <input type="text" name="${content}" id="${content}" /><br /><br />
+      <label for="title">제목</label><br />
+      <input type="text" name="title" id="title" /><br /><br />
+      <label for="content">내용</label><br />
+      <input type="text" name="content" id="content" /><br /><br />
       <input type="submit" value="포스팅 하기" />
     </form>
   </body>
 </html>
-`;
-  },
+`,
 };
 
 module.exports = blogStr;

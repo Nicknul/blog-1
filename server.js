61268dc183ccc260795f0a813e161a3daf7bc5e3
정호연 <jea7730@gmail.com>
2024-06-10 16:23:50 +0900

    blogStr.js 불러오기, 파일 쓰고 읽기 시도

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require('http');
 const fs = require('fs');
+const blogStr = require('./blogStr.js');
+console.log(blogStr.blog);
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/') {
     console.log('유효성 검사:', req.url);
+    fs.writeFileSync('./blog.html', blogStr.blog, 'utf-8');
+
+    let data = fs.readFileSync('./blog.html', 'utf-8');
+
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end(data);
   }
 });
 

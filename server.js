8f0332042cf60a3796c8f6b63e4be43369568186
정호연 <jea7730@gmail.com>
2024-06-10 17:23:35 +0900

    method POST 조건문 생성

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const http = require('http');
 const fs = require('fs');
+const qs = require('node:querystring');
 const blogStr = require('./blogStr.js');
-console.log(blogStr.blog);
 
 const server = http.createServer((req, res) => {
+  console.log('유효성 검사:', req.url);
   if (req.method === 'GET' && req.url === '/') {
-    console.log('유효성 검사:', req.url);
-    fs.writeFileSync('./blog.html', blogStr.blog, 'utf-8');
+    fs.writeFileSync('./blog.html', blogStr.main, 'utf-8');
 
     let data = fs.readFileSync('./blog.html', 'utf-8');
 
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
     res.end(data);
+  }
+  if (req.method === 'POST' && req.url === '/submit') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      let data = qs.parse(body);
+      let title = data.title;
+      let content = data.content;
+    });
   }
 });
 

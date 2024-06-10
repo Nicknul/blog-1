3055de7ef5ab804a0909fd821db97ee55c24b867
정호연 <jea7730@gmail.com>
2024-06-10 15:33:32 +0900

    server.listen 오타 수정, GET 요청 확인 완료

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    console.log('유효성 검사:', req.url);
+  }
+});
 
 const port = 8080;
 
 server.listen(port, () => {
   console.log('서버 돌아가는 중');
-  console.log(`http://localhost${port}`);
+  console.log(`http://localhost:${port}`);
 });

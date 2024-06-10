5fb82e4e8aa4e27fbe82b82b1650c4d384f45d38
정호연 <jea7730@gmail.com>
2024-06-10 16:23:15 +0900

    blog.html에 들어 갈 내용 수정, 펑션으로 변경 시도

--- blogStr.js
+++ blogStr.js
@@ -1,12 +1,24 @@
-const blogStr = `<!DOCTYPE html>
+const blogStr = {
+  blog: (title, content) => {
+    `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>BLOG</title>
   </head>
-  <body></body>
+  <body>
+    <form action="/submit" method="post">
+      <label for="${title}">제목</label><br />
+      <input type="text" name="${title}" id="${title}" /><br /><br />
+      <label for="${content}">내용</label><br />
+      <input type="text" name="${content}" id="${content}" /><br /><br />
+      <input type="submit" value="포스팅 하기" />
+    </form>
+  </body>
 </html>
 `;
+  },
+};
 
 module.exports = blogStr;

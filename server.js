82cefce2850d2f8b9101c973624ce023e83ec722
정호연 <jea7730@gmail.com>
2024-06-11 14:48:17 +0900

    replace로 조립한 데이터를 가지고 main.html를 다시 생성하기_완료

--- server.js
+++ server.js
@@ -23,27 +23,28 @@ const server = http.createServer((req, res) => {
       let data = qs.parse(body);
       let title = data.title;
       let content = data.content;
-
+      //todo-1 POS 데이터 받기가 끝난다면 변수 title, content의 내용을 포함한 html를 만드는 작업
       fs.writeFileSync(`./list/${title}.html`, string.create(title, content), 'utf-8');
 
-      // let frist = fs.readFileSync(`./list/${title}.html`, 'utf-8');
-
-      // res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-      // res.end(frist);
-
+      //todo-2 readdir 선언
       let list = fs.readdirSync('./list');
 
+      //todo-3 readdir의 목록을 for...in를 통해 html에 나타나게 하기
       let arr = [];
-
       for (let element in list) {
-        arr.push(`<li>${list[element]}</li>`);
+        arr.push(`<li><a href="${list[element]}">${list[element]}</a></li>`);
       }
-
+      //todo-4 string.main에 있는 문자열을 replace를 통해 readdir 값을 넣고 조립하기
       let mainAdd = string.main.replace(
         '</form>',
         `</form>
-        <div>${arr.join('')}</div>`
+        <ul>${arr.join('')}</ul>`
       );
+      //todo-5 POST 데이터 받기가 끝나면 조립한 데이터가 들어간 main.html을 다시 만드는 작업
+      fs.writeFileSync('./main.html', mainAdd, 'utf-8');
+      let second = fs.readFileSync('./main.html', 'utf-8');
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+      res.end(second);
     });
   }
 });
